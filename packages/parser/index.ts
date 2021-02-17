4fa49dd0fabecdaa0f96eba77875d12d088a9f3c
DuGlaser <duglaser0618@gmail.com>
2021-02-17 14:29:27 +0900

    :recycle: Fix the parser to use rehype

--- packages/parser/index.ts
+++ packages/parser/index.ts
@@ -1,16 +1,21 @@
+import React from 'react';
 import unified from 'unified';
-import parse from 'remark-parse';
-import remarkReact from 'remark-react';
+import toremark from 'remark-parse';
+import remarkTorehype from 'remark-rehype';
+import rehypeToreact, { ComponentOptions } from 'rehype-react';
 
-export const parser = (mdText: string): string => {
-  const result: string = unified()
-    .use(parse)
-    .use(remarkReact, {
-      remarkreactComponents: {
-        /** TODO: Add the custom component **/
-      },
+export const parser = (
+  mdText: string,
+  components?: ComponentOptions<any>['components']
+) => {
+  const result = unified()
+    .use(toremark)
+    .use(remarkTorehype)
+    .use(rehypeToreact, {
+      createElement: React.createElement,
+      components,
     })
-    .processSync(mdText).result as string;
+    .processSync(mdText).result as React.ReactElement;
 
   return result;
 };
