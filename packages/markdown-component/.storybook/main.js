03bf2598ed64bed45281c46bd10cd52d33ff265d
DuGlaser <duglaser0618@gmail.com>
2021-03-06 19:53:02 +0900

    :wrench: Fix the storybook setting

--- packages/markdown-component/.storybook/main.js
+++ packages/markdown-component/.storybook/main.js
@@ -2,4 +2,11 @@ module.exports = {
   stories: ['../components/**/*.stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   typescript: { reactDocgen: 'react-docgen' },
+  webpackFinal: (config) => {
+    config.resolve.modules.push(process.cwd() + '/node_modules');
+    config.resolve.modules.push(process.cwd() + '/components');
+
+    config.resolve.symlinks = false;
+    return config;
+  },
 };
