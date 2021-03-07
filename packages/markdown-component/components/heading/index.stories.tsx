9a3bdace65c3132b242a3c6dfbea995d9e993ff9
DuGlaser <duglaser0618@gmail.com>
2021-03-07 15:31:56 +0900

    :recycle: Fix the heading component

--- packages/markdown-component/components/heading/index.stories.tsx
+++ packages/markdown-component/components/heading/index.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meta } from '@storybook/react/types-6-0';
-import { H1 } from './index';
+import { H1, H2, H3, H4 } from './index';
 
 export default {
   title: 'markdown-component/Heading',
@@ -10,6 +10,9 @@ export const Heading = () => {
   return (
     <div>
       <H1>Heading</H1>
+      <H2>Heading</H2>
+      <H3>Heading</H3>
+      <H4>Heading</H4>
     </div>
   );
 };
--- packages/markdown-component/components/heading/index.tsx
+++ packages/markdown-component/components/heading/index.tsx
@@ -2,35 +2,53 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 const BaseHeading = `
-  
+  margin: 0;
+  padding: 0;
+
+  font-weight: bold;
+  margin-bottom: 0.5rem;
 `;
 
-const StyledH1 = styled.h1``;
+const StyledH1 = styled.h1`
+  ${BaseHeading}
+  font-size: 2rem;
+  line-height: 2.5rem;
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+`;
 
 export const H1: React.FC = ({ ...props }) => {
   return <StyledH1 {...props}>{props.children}</StyledH1>;
 };
 
-const StyledH2 = styled.h2``;
+const StyledH2 = styled.h2`
+  ${BaseHeading}
+  font-size: 1.5rem;
+  line-height: 2rem;
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+`;
 
 export const H2: React.FC = ({ ...props }) => {
   return <StyledH2 {...props}>{props.children}</StyledH2>;
 };
 
-const StyledH3 = styled.h3``;
+const StyledH3 = styled.h3`
+  ${BaseHeading}
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+`;
 
 export const H3: React.FC = ({ ...props }) => {
   return <StyledH3 {...props}>{props.children}</StyledH3>;
 };
 
-const StyledH4 = styled.h4``;
+const StyledH4 = styled.h4`
+  ${BaseHeading}
+  font-size: 1rem;
+  line-height: 1.5rem;
+`;
 
 export const H4: React.FC = ({ ...props }) => {
   return <StyledH4 {...props}>{props.children}</StyledH4>;
-};
-
-const StyledH5 = styled.h5``;
-
-export const H5: React.FC = ({ ...props }) => {
-  return <StyledH5 {...props}>{props.children}</StyledH5>;
 };
