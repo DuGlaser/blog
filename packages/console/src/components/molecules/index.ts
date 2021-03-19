82e21ccab205836e75124b71d8c0c9df6658d5a1
DuGlaser <duglaser0618@gmail.com>
2021-03-20 01:23:55 +0900

    :sparkles: Add the Editor, Preview component

--- packages/console/src/components/molecules/index.ts
+++ packages/console/src/components/molecules/index.ts
@@ -1,2 +1,4 @@
 export { Drawer } from '@/components/molecules/Drawer';
 export { LoggedinUser } from '@/components/molecules/LoggedinUser';
+export { Editor } from '@/components/molecules/Editor';
+export { Preview } from '@/components/molecules/Preview';
