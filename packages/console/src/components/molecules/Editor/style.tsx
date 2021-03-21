eb72ec3c3f950bfad4c35b1e5d5eee177d54c3bd
DuGlaser <duglaser0618@gmail.com>
2021-03-21 14:25:51 +0900

    :sparkles: Add the customScrollBar style

--- packages/console/src/components/molecules/Editor/style.tsx
+++ packages/console/src/components/molecules/Editor/style.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import { customScrollBar } from '@/utils/customScrollBar';
+
 export const Textarea = styled.textarea`
   height: 100%;
   width: 100%;
@@ -9,4 +11,6 @@ export const Textarea = styled.textarea`
   color: ${(props) => props.theme.color.gray};
   resize: none;
   outline: none;
+
+  ${customScrollBar}
 `;
--- packages/console/src/components/molecules/Preview/style.tsx
+++ packages/console/src/components/molecules/Preview/style.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import { customScrollBar } from '@/utils/customScrollBar';
+
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
@@ -7,5 +9,8 @@ export const Wrapper = styled.div`
   color: ${(props) => props.theme.color.gray};
   padding: 2rem;
 
-  overflow: hidden;
+  overflow-y: auto;
+  overflow-wrap: break-word;
+
+  ${customScrollBar}
 `;
