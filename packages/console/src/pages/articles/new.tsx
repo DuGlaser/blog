0589e99707bcb8c0515885b75fe3f8a18ee8b6a8
DuGlaser <duglaser0618@gmail.com>
2021-03-27 19:52:17 +0900

    :shower: Del the unnecessary code

--- packages/console/src/pages/articles/new.tsx
+++ packages/console/src/pages/articles/new.tsx
@@ -1,12 +1,8 @@
-import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import { NextPage } from 'next';
 import { AuthAction, withAuthUser } from 'next-firebase-auth';
-import { useCallback, useState } from 'react';
 
-import { FlatButton, OutlineButton } from '@/components/atoms';
-import { TextFilled } from '@/components/atoms/TextFilled';
-import { Editor, Preview } from '@/components/molecules';
+import { ArticleEditor } from '@/components/organisms';
 import { Layout } from '@/components/templates';
 
 const S = {
@@ -22,86 +18,15 @@ const S = {
   Wrapper: styled.div`
     height: 95%;
     width: 95%;
-    max-width: 1440px;
-
-    display: flex;
-    flex-direction: column;
-  `,
-
-  Header: styled.div`
-    display: flex;
-    align-items: center;
-
-    margin-bottom: 2rem;
-  `,
-
-  ButtonWrapper: styled.div`
-    & :first-child {
-      margin-right: 1rem;
-    }
-  `,
-
-  TitleWrapper: styled.div`
-    flex: 1;
-    margin: 0 2rem 0 0;
-  `,
-
-  Content: styled.div`
-    flex: 1;
-    width: 100%;
-    display: flex;
-
-    overflow-y: auto;
-  `,
-
-  Border: styled.div`
-    height: 100%;
-    width: 3px;
-    background-color: ${(props) => props.theme.color.base};
-    opacity: 0.3;
   `,
 };
 
 const NewPage: NextPage = () => {
-  const [editor, setEditor] = useState('');
-  const theme = useTheme();
-
-  const handleOnChange = useCallback((value) => {
-    setEditor(value);
-  }, []);
-
   return (
     <Layout>
       <S.Flex>
         <S.Wrapper>
-          <S.Header>
-            <S.TitleWrapper>
-              <TextFilled
-                textColor={theme.color.white}
-                placeholder={'Title'}
-                fontSize={'32px'}
-              />
-            </S.TitleWrapper>
-            <S.ButtonWrapper>
-              <OutlineButton
-                borderColor={theme.color.primary}
-                textColor={theme.color.primary}
-              >
-                情報
-              </OutlineButton>
-              <FlatButton
-                bgColor={theme.color.primary}
-                textColor={theme.color.white}
-              >
-                公開する
-              </FlatButton>
-            </S.ButtonWrapper>
-          </S.Header>
-          <S.Content>
-            <Editor value={editor} onChange={handleOnChange} />
-            <S.Border />
-            <Preview value={editor} />
-          </S.Content>
+          <ArticleEditor />
         </S.Wrapper>
       </S.Flex>
     </Layout>
