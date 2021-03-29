8c0c3bc389fafd55518d6be431b0286c1c97f590
DuGlaser <duglaser0618@gmail.com>
2021-03-29 23:08:40 +0900

    :art: Fix the Layout component style

--- packages/console/src/components/templates/Layout/style.tsx
+++ packages/console/src/components/templates/Layout/style.tsx
@@ -2,19 +2,11 @@ import styled from '@emotion/styled';
 
 const HEADER_HEIGHT = '64px';
 
-export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
-  height: 100vh;
-
-  background-color: ${(props) => props.theme.color.bgColor};
-`;
+export const Wrapper = styled.div``;
 
 export const Header = styled.header`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
 
   height: ${HEADER_HEIGHT};
   padding: 16px;
@@ -22,6 +14,24 @@ export const Header = styled.header`
   background-color: ${(props) => props.theme.color.base};
 `;
 
+export const Icon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100%;
+  aspect-ratio: 1/1;
+
+  font-size: 1.2rem;
+  color: ${(props) => props.theme.color.base};
+
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.color.white};
+
+  cursor: pointer;
+`;
+
 export const Content = styled.div`
-  height: calc(100vh - ${HEADER_HEIGHT});
+  min-height: calc(100vh - ${HEADER_HEIGHT});
+  height: 1px;
 `;
