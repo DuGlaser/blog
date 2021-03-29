9ef9a5be1edab74673b16efb29a9398c1e87f26d
DuGlaser <duglaser0618@gmail.com>
2021-03-29 16:30:19 +0900

    :sparkles: Add the useGetArticle hooks

---
+++ packages/console/src/hooks/useGetArticle.ts
@@ -0,0 +1,24 @@
+import 'firebase/firestore';
+
+import {
+  useCollectionDataOnce,
+  useDocumentData,
+} from 'react-firebase-hooks/firestore';
+
+import { Article, FirestoreArticle } from '@/types/article';
+import firebase from '@/utils/firebase';
+
+export const useGetArticlesDataOnce = () => {
+  return useCollectionDataOnce<Article>(
+    firebase.firestore().collection('articles'),
+    {
+      idField: 'id',
+    }
+  );
+};
+
+export const useGetArticleData = (id: string) => {
+  return useDocumentData<Article>(firebase.firestore().doc(`articles/${id}`), {
+    idField: 'id',
+  });
+};
