5af446bd5010164ddd88b3cdabc7460887be1583
DuGlaser <duglaser0618@gmail.com>
2021-03-31 16:06:05 +0900

    :sparkles: Add the updated_at, created_at to the firestore data

--- packages/console/src/hooks/useSaveArticle.ts
+++ packages/console/src/hooks/useSaveArticle.ts
@@ -8,12 +8,19 @@ import firebase from '@/utils/firebase';
 export const useSaveArticle = (id = '') => {
   const [docId, setDocId] = useState(id);
 
-  const handleSave = async (article: FirestoreArticle) => {
+  const handleSave = async (article: Partial<FirestoreArticle>) => {
     const db = firebase.firestore();
+    const now = Date.now();
+
     if (docId) {
+      article.updated_at = now;
+
       const ref = db.collection('articles').doc(docId);
       await ref.set(article, { merge: true });
     } else {
+      article.updated_at = now;
+      article.created_at = now;
+
       const ref = await db.collection('articles').add(article);
       setDocId(ref.id);
     }
--- packages/console/src/types/article.ts
+++ packages/console/src/types/article.ts
@@ -3,6 +3,8 @@ export type Article = {
   public: boolean;
   title: string;
   body: string;
+  updated_at: number;
+  created_at: number;
 };
 
 export type FirestoreArticle = Omit<Article, 'id'>;
