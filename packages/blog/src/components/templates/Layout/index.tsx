480e5e139c21acb4db07418a9cc53da09cccc13b
DuGlaser <duglaser0618@gmail.com>
2021-04-03 20:40:20 +0900

    :recycle: Add the Layout/style.tsx

--- packages/blog/src/components/templates/Layout/index.tsx
+++ packages/blog/src/components/templates/Layout/index.tsx
@@ -1,41 +1,14 @@
-import styled from '@emotion/styled';
-
 import { Header } from '@/components/atoms';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-const Content = styled.main`
-  flex: 1;
-
-  width: 100%;
-  max-width: 1024px;
-
-  margin: 0 auto;
-  padding: 16px;
-`;
-
-const Footer = styled.div`
-  margin-top: auto;
-  padding: 1rem 0;
-
-  text-align: center;
-  color: ${(props) => props.theme.color.gray};
-
-  width: 100%;
-`;
+import * as S from './style';
 
 export const Layout: React.FC = ({ children }) => {
   return (
-    <Wrapper>
+    <S.Wrapper>
       <Header />
-      <Content>{children}</Content>
-      <Footer>
+      <S.Content>{children}</S.Content>
+      <S.Footer>
         <small>© 2020 DuGlaser</small>
-      </Footer>
-    </Wrapper>
+      </S.Footer>
+    </S.Wrapper>
   );
 };
