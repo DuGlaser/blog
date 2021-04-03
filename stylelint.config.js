aafb2d0718aba6a3f6fc86daa0cd5bf412d6e461
DuGlaser <duglaser0618@gmail.com>
2021-04-03 21:25:20 +0900

    :wrench: Add the stylelint setting

---
+++ stylelint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-order', 'stylelint-config-rational-order/plugin'],
+  rules: {
+    'value-keyword-case': false,
+    // Property Order
+    'order/properties-order': [[], { severity: 'warning' }],
+    'plugin/rational-order': [
+      true,
+      {
+        'border-in-box-model': false,
+        'empty-line-between-groups': false,
+        severity: 'warning',
+      },
+    ],
+  },
+};
