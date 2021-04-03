7344770c0c937f028e609c0ab1d4959a0d272b67
DuGlaser <duglaser0618@gmail.com>
2021-04-03 19:23:28 +0900

    :sparkles: Add the site footer

--- packages/blog/src/components/templates/Layout/index.tsx
+++ packages/blog/src/components/templates/Layout/index.tsx
@@ -2,7 +2,15 @@ import styled from '@emotion/styled';
 
 import { Header } from '@/components/atoms';
 
-const Content = styled.div`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Content = styled.main`
+  flex: 1;
+
   width: 100%;
   max-width: 1024px;
 
@@ -10,11 +18,24 @@ const Content = styled.div`
   padding: 16px;
 `;
 
+const Footer = styled.div`
+  margin-top: auto;
+  padding: 1rem 0;
+
+  text-align: center;
+  color: ${(props) => props.theme.color.gray};
+
+  width: 100%;
+`;
+
 export const Layout: React.FC = ({ children }) => {
   return (
-    <>
+    <Wrapper>
       <Header />
       <Content>{children}</Content>
-    </>
+      <Footer>
+        <small>© 2020 DuGlaser</small>
+      </Footer>
+    </Wrapper>
   );
 };
