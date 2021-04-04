3be99afc70c436eed5f80d503febd4889dafb971
DuGlaser <duglaser0618@gmail.com>
2021-04-04 18:19:50 +0900

    :recycle: Fix with eslint

--- packages/console/src/components/atoms/index.ts
+++ packages/console/src/components/atoms/index.ts
@@ -1,6 +1,6 @@
 export { Avatar } from '@/components/atoms/Avatar';
 export { FlatButton } from '@/components/atoms/FlatButton';
 export { FloatingWindow } from '@/components/atoms/FloatingWindow';
+export { ModalDecorator } from '@/components/atoms/ModalDecorator';
 export { OutlineButton } from '@/components/atoms/OutlineButton';
 export { TextFilled } from '@/components/atoms/TextFilled';
-export { ModalDecorator } from '@/components/atoms/ModalDecorator';
