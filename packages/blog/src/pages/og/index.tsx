7d6787d9052a826e547155e17f80c5f5597761bf
DuGlaser <duglaser0618@gmail.com>
2021-04-08 22:15:41 +0900

    :recycle: Change the api/og to not use next.js api routing

---
+++ packages/blog/src/pages/og/index.tsx
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { useRouter } from 'next/router';
+import path from 'path';
+
+import { theme } from '@/utils/theme';
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const fontPath = path.resolve(
+    process.cwd(),
+    './assets/DelaGothicOne-Regular.ttf'
+  );
+  const font = fs.readFileSync(fontPath, { encoding: 'base64' });
+
+  return {
+    props: {
+      font: font,
+    },
+  };
+};
+
+const OgPage: NextPage<{ font: string }> = ({ font }) => {
+  const router = useRouter();
+  const { title } = router.query;
+  return (
+    <>
+      <style jsx>{`
+        @font-face {
+          font-weight: bold;
+          font-family: 'Dela Gothic One';
+          src: url(data:font/ttf;charset=utf-8;base64,${font})
+            format('truetype');
+        }
+
+        div {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+          padding: 0 10%;
+          font-family: 'Dela Gothic One', cursive;
+          background-color: ${theme.color.bgColor};
+        }
+
+        h1 {
+          margin: 0 auto;
+          color: ${theme.color.white};
+          font-size: 3.5rem;
+          letter-spacing: 0.1rem;
+        }
+      `}</style>
+      <div>
+        <h1>{title}</h1>
+      </div>
+    </>
+  );
+};
+
+export default OgPage;
