84c8b8df01c95365cf6a3c32285d30c4a2e0787f
DuGlaser <duglaser0618@gmail.com>
2021-04-15 14:24:53 +0900

    :sparkles: Add the markdownStyleMixin

--- packages/blog/src/components/molecules/ArticleContent/style.tsx
+++ packages/blog/src/components/molecules/ArticleContent/style.tsx
@@ -1,61 +1,12 @@
 import styled from '@emotion/styled';
+import { markdownStyleMixin } from '@blog/util';
 
 const markdownStyle = styled.div`
-  color: ${(props) => props.theme.color.lightGrey};
-  letter-spacing: 0.05em;
-
-  h1,
-  h2,
-  h3,
-  h4 {
-    margin-top: 2.3em;
-    margin-bottom: 0.5em;
-    padding-bottom: 0.4em;
-  }
-
-  h1 {
-    font-size: 1.7rem;
-    -webkit-background-clip: padding-box;
-    background-clip: padding-box;
-    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-  }
-  h2 {
-    font-size: 1.5rem;
-    -webkit-background-clip: padding-box;
-    background-clip: padding-box;
-    border-bottom: 1px solid rgba(255, 255, 255, 0.2);
-  }
-
-  a {
-    color: ${(props) => props.theme.color.primary};
-    text-decoration: none;
-    word-break: break-all;
-
-    :hover {
-      text-decoration: underline;
-    }
-  }
-
-  p {
-    margin-top: 1em;
-    font-size: 1rem;
-    line-height: 2em;
-  }
-
-  li {
-    margin: 0.5em 0;
-  }
-
-  ul,
-  ol {
-    margin: 1.4rem 0;
-  }
-
-  blockquote {
-    margin: 1.4rem 0;
-    padding-left: 1rem;
-    border-left: 3px solid ${(props) => props.theme.color.primary};
-  }
+  ${(props) =>
+    markdownStyleMixin({
+      textColor: props.theme.color.lightGrey,
+      primaryColor: props.theme.color.primary,
+    })}
 `;
 
 export const Title = styled.h1`
