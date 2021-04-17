c7ec9776f59fd3a157a5b96edf8078050d8ed152
DuGlaser <duglaser0618@gmail.com>
2021-04-17 19:48:15 +0900

    :sparkles: Add the imageSize func

--- packages/parser/index.ts
+++ packages/parser/index.ts
@@ -1,7 +1,6 @@
 import React from 'react';
 import rehypeToreact from 'rehype-react';
 import remark from 'remark';
-import attr from 'remark-attr';
 import breaks from 'remark-breaks';
 import gfm from 'remark-gfm';
 import toremark from 'remark-parse';
@@ -9,12 +8,14 @@ import remarkTorehype from 'remark-rehype';
 import strip from 'strip-markdown';
 import unified from 'unified';
 
+import { imageSize } from './src/imageSize';
+
 export const parser = (mdText: string, components?: { [key: string]: any }) => {
   const element = unified()
     .use(toremark)
-    .use(attr)
     .use(gfm)
     .use(breaks)
+    .use(imageSize)
     .use(remarkTorehype)
     .use(rehypeToreact, {
       createElement: React.createElement,
