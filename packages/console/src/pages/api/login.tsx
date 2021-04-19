ce4fe533503a676fc94fd59d2d0f27af089e3940
DuGlaser <duglaser0618@gmail.com>
2021-04-19 22:56:36 +0900

    :recycle: Fix the login api

--- packages/console/src/pages/api/login.tsx
+++ packages/console/src/pages/api/login.tsx
@@ -43,6 +43,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     return res.status(200).json({ success: true });
   } catch (e) {
+    console.log(e);
     return res.status(500).json({ error: 'Unexpected error.' });
   }
 };
