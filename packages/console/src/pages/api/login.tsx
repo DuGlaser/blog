e7853012e622bd50d9a4e5e2a3718e11433a2b08
DuGlaser <duglaser0618@gmail.com>
2021-04-19 10:19:46 +0900

    :hankey: Debug

--- packages/console/src/pages/api/login.tsx
+++ packages/console/src/pages/api/login.tsx
@@ -36,7 +36,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       return res.status(200).json({ success: true });
     }
 
-    if (AuthUser.id !== process.env.FIREBASE_ADMIN_ID) {
+    if (AuthUser.id === process.env.FIREBASE_ADMIN_ID) {
       await sendLog(AuthUser);
       return res.status(200).json({ success: true });
     }
