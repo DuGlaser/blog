d53223114e0cc27584457b6d2a657011dfaec9c1
DuGlaser <duglaser0618@gmail.com>
2021-04-20 00:05:31 +0900

    :shower: Del the unnecessary code

--- packages/console/src/pages/api/login.tsx
+++ packages/console/src/pages/api/login.tsx
@@ -20,10 +20,7 @@ const sendLog = async (log: any) => {
   };
 
   if (process.env.DISCORD_WEB_HOOK) {
-    const res = await axios.post(process.env.DISCORD_WEB_HOOK, message, config);
-    console.log({ res });
-  } else {
-    console.log('DISCORD_WEB_HOOK is undefined');
+    await axios.post(process.env.DISCORD_WEB_HOOK, message, config);
   }
 };
 
@@ -31,19 +28,13 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { AuthUser } = await setAuthCookies(req, res);
 
-    if (!process.env.FIREBASE_ADMIN_ID) {
-      console.log('FIREBASE_ADMIN_ID is undefined');
-      return res.status(200).json({ success: true });
-    }
-
-    if (AuthUser.id === process.env.FIREBASE_ADMIN_ID) {
+    if (AuthUser.id !== process.env.FIREBASE_ADMIN_ID) {
       await sendLog(AuthUser);
-      return res.status(200).json({ success: true });
+      return res.status(500).json({ error: 'Unauthorized user.' });
     }
 
     return res.status(200).json({ success: true });
   } catch (e) {
-    console.log(e);
     return res.status(500).json({ error: 'Unexpected error.' });
   }
 };
